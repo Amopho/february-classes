fa2b482dd995ffcc31ae61fde97932de2caaf6c5
Amopho <amopho78@gmail.com>
2021-02-18 13:49:51 +0100

    post test

---
+++ 20210218_after_test/index.js
@@ -0,0 +1,36 @@
+// Word converter
+// const wordConverter = (arr, suffix);
+
+// Hour tracking
+
+// const hourTracking = [
+//     {day: "Monday", start: 8, end: 17},
+//     {day: "Tuesday", start: 8, end: 17},
+//     {day: "Wednesday", start: 8, end: 17},
+//     {day: "Thursday", start: 8, end: 17},
+//     {day: "MonFridayday", start: 8, end: 17},
+// ];
+
+// const calc
+
+//Classes
+
+class Course = {
+    constructor(_teacher, _type, _number) {
+        this.teacher = _teacher;
+        this.type = _type;
+        this.number = _number;
+    }
+}
+// 
+
+function upperCase() {
+
+}
+
+// Validate Pin
+
+
+
+// wherever you split() upon some criteria, you have to join() with the same criteria
+
